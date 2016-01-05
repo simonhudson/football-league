985496b57d68e13835997c6a66795df84674f4cc
Simon Hudson <simon.hudson@experian.com>
2016-01-05 09:48:56 +0000

    Footer spacing

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,4 +1,51 @@
 /*
+Main navigation
+*/
+var MainNav = {
+
+    showNavAt: 1024,
+
+    init: function() {
+        var mainNavObj = {
+            nav: $('.main-nav'),
+            toggle: $('.main-nav__toggle')
+        };
+
+        if (Window.getViewportWidth() >= MainNav.showNavAt)
+            MainNav.showNav(mainNavObj);
+
+        MainNav.eventListeners(mainNavObj);
+    },
+
+    eventListeners: function(mainNavObj) {
+        MainNav.setState(mainNavObj); // On load
+
+        Window.resize(function() {
+            MainNav.setState(mainNavObj);
+        });
+    },
+
+    setState: function(mainNavObj) {
+        if (Window.getViewportWidth() >= MainNav.showNavAt) {
+            mainNavObj.toggle.attr('tabindex', '-1');
+            MainNav.showNav(mainNavObj);
+        } else {
+            mainNavObj.toggle.removeAttr('tabindex');
+            MainNav.hideNav(mainNavObj);
+        }
+    },
+
+    showNav: function(mainNavObj) {
+        mainNavObj.nav.show();
+    },
+
+    hideNav: function(mainNavObj) {
+        mainNavObj.nav.hide();
+    },
+
+};
+$(document).ready(MainNav.init);
+/*
 Show/hide
 */
 
@@ -87,50 +134,3 @@ var Window = {
     }
     
 };
-/*
-Main navigation
-*/
-var MainNav = {
-
-    showNavAt: 1024,
-
-    init: function() {
-        var mainNavObj = {
-            nav: $('.main-nav'),
-            toggle: $('.main-nav__toggle')
-        };
-
-        if (Window.getViewportWidth() >= MainNav.showNavAt)
-            MainNav.showNav(mainNavObj);
-
-        MainNav.eventListeners(mainNavObj);
-    },
-
-    eventListeners: function(mainNavObj) {
-        MainNav.setState(mainNavObj); // On load
-
-        Window.resize(function() {
-            MainNav.setState(mainNavObj);
-        });
-    },
-
-    setState: function(mainNavObj) {
-        if (Window.getViewportWidth() >= MainNav.showNavAt) {
-            mainNavObj.toggle.attr('tabindex', '-1');
-            MainNav.showNav(mainNavObj);
-        } else {
-            mainNavObj.toggle.removeAttr('tabindex');
-            MainNav.hideNav(mainNavObj);
-        }
-    },
-
-    showNav: function(mainNavObj) {
-        mainNavObj.nav.show();
-    },
-
-    hideNav: function(mainNavObj) {
-        mainNavObj.nav.hide();
-    },
-
-};
-$(document).ready(MainNav.init);
